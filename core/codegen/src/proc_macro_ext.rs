b9c57e36096fb20c08f7190bf71708dcfce464c8
Jesper Steen Møller <jesper@selskabet.org>
2019-07-09 20:56:59 +0200

    Fix compilation errors with nightly-2019-08-01

--- core/codegen/src/proc_macro_ext.rs
+++ core/codegen/src/proc_macro_ext.rs
@@ -1,7 +1,5 @@
 use proc_macro::{Diagnostic, Literal, Span};
-use std::ops::{Bound, Deref, RangeBounds};
-
-use syntax_pos::{BytePos, Pos, Span as InnerSpan};
+use std::ops::{Deref, RangeBounds};
 
 pub type PResult<T> = ::std::result::Result<T, Diagnostic>;
 
@@ -46,46 +44,6 @@ impl From<Diagnostic> for Diagnostics {
 impl From<Vec<Diagnostic>> for Diagnostics {
     fn from(diags: Vec<Diagnostic>) -> Self {
         Diagnostics(diags)
-    }
-}
-
-pub trait SpanExt {
-    fn subspan<R: RangeBounds<usize>>(self, range: R) -> Option<Span>;
-}
-
-impl SpanExt for Span {
-    /// Create a `subspan` from `start` to `end`.
-    fn subspan<R: RangeBounds<usize>>(self, range: R) -> Option<Span> {
-        let inner: InnerSpan = unsafe { ::std::mem::transmute(self) };
-        let length = inner.hi().to_usize() - inner.lo().to_usize();
-
-        let start = match range.start_bound() {
-            Bound::Included(&lo) => lo,
-            Bound::Excluded(&lo) => lo + 1,
-            Bound::Unbounded => 0,
-        };
-
-        let end = match range.end_bound() {
-            Bound::Included(&hi) => hi + 1,
-            Bound::Excluded(&hi) => hi,
-            Bound::Unbounded => length,
-        };
-
-        // Bounds check the values, preventing addition overflow and OOB spans.
-        if start > u32::max_value() as usize
-            || end > u32::max_value() as usize
-            || (u32::max_value() - start as u32) < inner.lo().to_u32()
-            || (u32::max_value() - end as u32) < inner.lo().to_u32()
-            || start >= end
-            || end > length
-        {
-            return None;
-        }
-
-        let new_lo = inner.lo() + BytePos(start as u32);
-        let new_hi = inner.lo() + BytePos(end as u32);
-        let new_inner = inner.with_lo(new_lo).with_hi(new_hi);
-        Some(unsafe { ::std::mem::transmute(new_inner) })
     }
 }
 
