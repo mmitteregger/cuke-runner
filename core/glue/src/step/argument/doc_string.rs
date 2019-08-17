15a69546ffef6c21f21628b0bf6e867e40d1f348
Michael Mitteregger <michael.mitteregger@gmx.at>
2019-08-17 10:49:47 +0200

    Add getters for content_type and line of DocString

--- core/glue/src/step/argument/doc_string.rs
+++ core/glue/src/step/argument/doc_string.rs
@@ -36,8 +36,56 @@ pub struct DocString<'s> {
 }
 
 impl<'s> DocString<'s> {
+    /// Returns the parsed value (arguments already replaced) from the doc string.
     pub fn value(&self) -> &str {
         &self.cuke_string.content
+    }
+
+    /// Returns the optional content type of the doc string.
+    ///
+    /// # Examples
+    ///
+    /// With the step:
+    /// ```gherkin
+    ///   Then the result is:
+    ///   """
+    ///   6
+    ///   """
+    /// ```
+    /// The returned content type is `None`.
+    ///
+    /// But when the step is:
+    /// ```gherkin
+    ///   Then the result is:
+    ///   """text
+    ///   6
+    ///   """
+    /// ```
+    /// The returned content type is `Some("text")`.
+    pub fn content_type(&self) -> Option<&str> {
+        self.cuke_string.content_type
+            .as_ref()
+            .map(|content_type| content_type.as_ref())
+    }
+
+    /// Returns the line of the doc string start delimiter.
+    ///
+    /// The line in the feature file where the doc string start delimiter `"""` is declared,
+    /// which is also the place where the content type may be specified.
+    ///
+    /// # Examples
+    ///
+    /// With the line numbers of the step:
+    /// ```text
+    /// 20|  Then the result is:
+    /// 21|  # DocStrings may be useful for rather small multiline text (e.g. json)
+    /// 22|  """text
+    /// 23|  6
+    /// 24|  """
+    /// ```
+    /// The returned line is `22`.
+    pub fn line(&self) -> u32 {
+        self.cuke_string.location.line
     }
 }
 
